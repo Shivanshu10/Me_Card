84e21dcaba6816da35f7141deb8ef38c52855897
Shivanshu Mishra <smishra_be20@thapar.edu>
2021-12-27 21:42:32 +0530

    completed project

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,94 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+  runApp(HomePage());
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MiCard(),
+    );
+  }
+}
+
+class MiCard extends StatefulWidget {
+  @override
+  _MiCardState createState() => _MiCardState();
+}
+
+class _MiCardState extends State<MiCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.teal,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage("assets/images/avatar.jpeg"),
+            ),
+            Text("Shivanshu Mishra",
+              style: TextStyle(
+                fontSize: 40.0,
+                fontFamily: 'Pacifico',
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              )
+            ),
+            Text("FLUTTER DEVELOPER",
+              style: TextStyle(
+                fontSize: 20.0,
+                fontFamily: 'Source Sans Pro',
+                color: Colors.teal.shade100,
+                letterSpacing: 2.5,
+                fontWeight: FontWeight.bold,
+              )
+            ),
+            SizedBox(
+              height: 20.0,
+              width: 150.0,
+              child: Divider(
+                color: Colors.teal.shade100,
+              )
+            ),
+            ContactInfoCard(icon: Icons.phone, text: '+91 9876543210'),
+            ContactInfoCard(icon: Icons.email, text: 'mailid@gmail.edu')
+          ],
+        )
+      )
+    );
+  }
+}
+
+class ContactInfoCard extends StatelessWidget {
+  final icon, text;
+  ContactInfoCard({this.icon, this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 90.0,
+      child: Card(
+        color: Colors.white,
+        margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+        child: Padding(
+          padding: EdgeInsets.all(10.0),
+          child: ListTile(
+            leading: Icon(icon, color: Colors.teal),
+            title: Text(text,
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.teal.shade900,
+                fontFamily: "Source Sans Pro"
+              )
+            ),
+          )
+        )
       ),
-    ),
-  );
+    );
+  }
 }
